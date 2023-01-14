0c799c7c7c3ac9337c65e1489a91b8ef52e2fbe0
tommyc-749133245 <tommy1002002@hotmail.com>
2023-01-14 09:53:39 +0800

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,12 @@
+#Tom
+name = """ 
+ TTTTT   OOO   M   M  M   M  Y    Y
+   T    O   O  MM MM  MM MM   Y  Y
+   T    O   O  MM MM  MM MM     Y
+   T    O   O  M M M  M M M     Y
+   T    O   O  M   M  M   M     Y
+   T    O   O  M   M  M   M     Y
+   T     OOO   M   M  M   M     Y
+"""
+
+print(name)
